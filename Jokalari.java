be1cf42f4e2f6167091cf43e312fa71c9ec93119
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2024-04-23 19:14:10 +0200

    Update Jokalari.java

--- Jokalari.java
+++ Jokalari.java
@@ -1,6 +1,66 @@
 package UnoProiektua;
+import java.util.ArrayList;
+import java.util.Iterator;
 
 public class Jokalari {
+	private String id;
+	private KartaZerrenda eskua;
+	
+	public Jokalari(String pId)
+	{
+		this.id = pId;
+		this.eskua = new KartaZerrenda();
+	}
+	
+	
+	public void jolastuTurnoa()
+	{
+		boolean jolastu = true;
+		while(jolastu == true)
+		{	
+			if(eskua==null)
+			{
+				for (int i=0; i<5; i++)
+				{
+					Karta k = kartaHartu();
+					eskua.add(k);
+				}	
+			}
+			if(kartaBotaBai == true)
+			{
+				int botatzeko = Teklatua.getInt();
+				Karta bota = eskua.get(botatzeko);
+				kartaEman(bota);
+				jolastu = false;
+			}
+			else
+			{
+				kartaHartu();
+			}
+		}
+	}
+	
+	private Karta kartaHartu()
+	{
+		Karta k = Mazoa.kartaEman();
+		return k;
+	}
+	
+	private boolean kartaBotaBai()
+	{
+		Karta erdiko = Mahaia.kartaEman();
+		boolean bota = eskua.berdina(erdiko);
+		return bota;
+	}
+	
+	private void kartaEman(Karta pKarta)
+	{
+		Mahaia.gehituKarta(pKarta);
+	}
 
-	public void jolastuTurnoa() {}
 }
+
+//zenbat adliz hartu daiteke kartak?
+//errore bat egotekotan ez dago amaitua
+//zergatik get txarto??
+//kartaBotaBai KartaZerrendan egin behar da
