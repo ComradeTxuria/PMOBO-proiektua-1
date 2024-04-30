83fbb39832d81a7343cfef1410a65ff09a6f66da
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2024-05-01 00:00:40 +0200

    Update Mahaia.java

--- Mahaia.java
+++ Mahaia.java
@@ -23,6 +23,16 @@ public class Mahaia {
 		return k;
 
 	}
+
+	public KartaZerrenda itzuliKartak()
+	{
+		KartaZerrenda itzultzeko = this.mKarta;
+		Karta k = itzuliKarta();
+		this.mKarta = null;
+		mKarta.gehituKarta(k);
+		itzultzeko.kenduKarta(itzultzeko.kartaKop()-1);
+		return itzultzeko;
+	}
 	
 	public boolean botaBai(Karta pKarta)
 	{
