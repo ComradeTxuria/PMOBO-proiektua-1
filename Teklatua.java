1708ae6cdd1a28ac64291bf50c4163f3d408a8a4
ComradeTxuria <164156208+ComradeTxuria@users.noreply.github.com>
2024-05-02 19:25:21 +0200

    Update Teklatua.java

--- Teklatua.java
+++ Teklatua.java
@@ -1,18 +1,16 @@
 import java.util.Scanner;
 
+import java.util.Scanner;
+
 public class Teklatua {
 	
 	private Scanner sc;
 	private static Teklatua nireTeklatua = null;
 	
-	private Teklatua() {
+	private Teklatua() 
+	{
 		sc = new Scanner(System.in);
 	}
-	
-	/*public Teklatua(Scanner pSc) {
-		
-		this.sc = pSc;
-	}*/
 	
 	public static Teklatua getTeklatua() {
 		
@@ -22,13 +20,15 @@ public class Teklatua {
 		return nireTeklatua;
 	}
 	
-	public String getString(String pMezua) {
+	public String getString(String pMezua) 
+	{
 		Printeatzea.getNirePrinteatzea().printTestua(pMezua);
 		String mezua = sc.nextLine();
 		return mezua;
 	}
 	
-	public int getInt(String pMezua) {
+	public int getInt(String pMezua) 
+	{
 		Printeatzea.getNirePrinteatzea().printTestua(pMezua);
 		int zenb = sc.nextInt();
 		sc.nextLine();
@@ -36,6 +36,3 @@ public class Teklatua {
 	}
 }
 
-
-
-//Meter los cambios
